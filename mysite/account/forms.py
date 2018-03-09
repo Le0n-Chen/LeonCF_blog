7dfb9332689fadecb3aedf74fcdfb0f10e8d9198
LeonCF <1037299165@qq.com>
2018-03-08 23:03:13 -0800

    重置密码

--- mysite/account/forms.py
+++ mysite/account/forms.py
@@ -1,18 +1,25 @@
 from django import forms
 from django.contrib.auth.models import User
+from .models import UserProfile
 
 class LoginForm(forms.Form):
     username=forms.CharField()
     password=forms.CharField(widget=forms.PasswordInput)
 
-class RegistrationForm(froms.ModelForm):
-	password=forms.CharField(label="Password",widget=forms.PasswordInput)
-	password2=forms.CharField(label="Confirm Password",widget=forms.PasswordInput)
-	class Meta:
-		models=User
-		fields=("username","email")
+class RegistrationForm(forms.ModelForm):
+    password=forms.CharField(label="Password",widget=forms.PasswordInput)
+    password2=forms.CharField(label="Confirm Password",widget=forms.PasswordInput)
     def clean_password2(self):
-    	cd=self.cleaned_data
-    	if cd['password']!=cd['password2']:
-    		raise forms.ValidationError("Passwords do not match")
-    	return cd['password2']
+        cd=self.cleaned_data
+        if cd['password']!=cd['password2']:
+            raise forms.ValidationError("passwords do not match")
+        return cd['password2']
+    class Meta:
+        model=User
+        fields=("username","email")
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model=UserProfile
+        fields=("phone","birth")
+    

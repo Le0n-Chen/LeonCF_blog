7758b028cccc8c17b240a1dee550e4bd29bf5000
LeonCF <1037299165@qq.com>
2018-03-17 20:51:45 -0700

    文章列表功能实现（next_查看文章）

--- mysite/article/urls.py
+++ mysite/article/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns=[
     url(r'^article-column/$',views.article_column,name="article_column"),
     url(r'^rename-column/$',views.rename_article_column,name="rename_article_column"),
+    url(r'^del-column/$',views.del_article_column,name="del_article_column"),
+    url(r'^article-post/$',views.article_post,name="article_post"),
+    url(r'^article-list/$',views.article_list,name="article_list"),
 ]
